2e2ce55677454cc4cf832840e8e5b2a619cf0412
hernandp <hernan.di.pietro@gmail.com>
2025-02-26 16:16:56 -0300

    deployConfigureMainnet.sh: added script call.

--- ethereum/ts-scripts/relayer/shell/deployConfigureMainnet.sh
+++ ethereum/ts-scripts/relayer/shell/deployConfigureMainnet.sh
@@ -5,4 +5,5 @@ npx tsx ./ts-scripts/relayer/config/checkNetworks.ts \
   && npx tsx ./ts-scripts/relayer/wormholeRelayer/deployWormholeRelayer.ts \
   && npx tsx ./ts-scripts/relayer/deliveryProvider/initializeDeliveryProvider.ts \
   && npx tsx ./ts-scripts/relayer/mockIntegration/deployMockIntegration.ts \
-  && npx tsx ./ts-scripts/relayer/wormholeRelayer/registerChainsWormholeRelayer.ts
+  && npx tsx ./ts-scripts/relayer/wormholeRelayer/registerChainsWormholeRelayer.ts \
+  && npx tsx ./ts-scripts/relayer/deliveryProvider/configureDeliveryProviderPriceAssistant.ts 
